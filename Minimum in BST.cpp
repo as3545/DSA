c0fd60c18d6e197012a47332cdbdf915d9364ac6
as3545 <86479682+as3545@users.noreply.github.com>
2023-09-02 14:26:20 +0530

    Minimum in BST.cpp

---
+++ Minimum in BST.cpp
@@ -0,0 +1,10 @@
+//Codeint minValue(Node* root) {
+    // Code here
+    if(root==NULL){
+        return -1;
+    }
+    while(root->left){
+        root=root->left;
+    }
+    return root->data;
+}
